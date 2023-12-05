6095c92b5372768311409e079892701b32e8ea91
bassy1992 <wyarquah@gmail.com>
2023-12-05 15:56:38 -0500

    update

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-""" Takes in a URL and sends a request to the URL
-and displays the body of the response
+"""A script that
+- takes in a URL
+- sends a request to the URL
+- displays the body of the response.
 """
-
 import sys
 import requests
 
-if __name__ == "__main__":
-        url = sys.argv[1]
 
-            response = requests.get(url)
-                if response.status_code >= 400:
-                            print("Error code: {}".format(response.status_code))
-                                else:
-                                            print(response.text)
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
-"""Takes in a letter and sends a POST request to
-http://0.0.0.0:5000/search_user with the letter as a parameter
+"""A script tha:
+- takes in a letter
+- sends POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
 """
-
 import sys
 import requests
 
+
 if __name__ == "__main__":
-        if len(sys.argv) == 1:
-                letter = ""
-                    else:
-                            letter = sys.argv[1]
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
 
-                                payload = {'q': letter}
-
-                                    request = requests.post('http://0.0.0.0:5000/search_user', data=payload)
-
-                                        try:
-                                                response = request.json()
-                                                        if response == {}:
-                                                                    print('No result')
-                                                                            else:
-                                                                                        print('[{}] {}'.format(response.get('id'), response.get('name')))
-                                                                                            except ValueError:
-                                                                                                    print('Not a valid JSON')
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    try:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
